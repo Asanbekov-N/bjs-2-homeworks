6f5ff224659b82e53c3fb848763df7a42e2fad91
Asanbekov-N <80524722+Asanbekov-N@users.noreply.github.com>
2021-07-01 13:22:30 +0300

    Update task.js
    home work

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,43 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+  }
+  if (d == 0) {
+    arr.push((b * -1) / (2 * a));
+  }
+
+  if (d > 0) {
+    arr.push(((b * -1) + Math.sqrt(d)) / (2 * a));
+    arr.push(((b * -1) - Math.sqrt(d)) / (2 * a));
+  }
+
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let toCreditDate = new Date(date);
+  let today = new Date();
+  var months = toCreditDate.getMonth() - today.getMonth() + ((toCreditDate.getFullYear() - today.getFullYear()) * 12);
+  let creditBody = amount - contribution;
+  let oneTwelve = (percent / 12) / 100;
+  var payPerMonth = creditBody * (oneTwelve + oneTwelve / (Math.pow((1 + oneTwelve), months) - 1));
+  let totalAmountTest = payPerMonth * months;
+  totalAmount = +totalAmountTest.toFixed(2)
 
-  // код для задачи №2 писать здесь
+  if (isNaN(Number(percent))) {
+    totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+    return totalAmount;
+  }
+  if (isNaN(Number(contribution))) {
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  }
+  if (isNaN(Number(amount))) {
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
 
   return totalAmount;
 }
