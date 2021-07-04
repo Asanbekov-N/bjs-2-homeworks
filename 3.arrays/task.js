70847a3c6b51cff3b2d1ed64c18d76bfb37e94c1
Asanbekov-N <80524722+Asanbekov-N@users.noreply.github.com>
2021-07-04 20:56:43 +0300

    Update task.js
    Logic changed.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,21 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result = false;
-
-  result = arr1.every(function (element) {
-    if (arr2[element] != arr1[element]) {
-      return false;
-    } else {
-      return true;
-    }
-  });
-
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((item) => arr1[item] === arr2[item]); // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr = [];
-
-  resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10)
-
-  return resultArr; // array
+  return arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10); // array
 }
