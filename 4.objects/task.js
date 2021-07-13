70040858965068f7c40eddff20f255809342797f
Asanbekov-N <80524722+Asanbekov-N@users.noreply.github.com>
2021-07-13 17:31:06 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,7 +1,11 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age
 }
+
+const student1 = new Student("Алексей", "мужской", "24");
+const student2= new Student("Алёна", "женский", "23");
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
