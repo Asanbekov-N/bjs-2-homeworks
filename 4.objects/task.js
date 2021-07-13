ff67081be4f91535b8d03afc85577dabcc359d5d
Asanbekov-N <80524722+Asanbekov-N@users.noreply.github.com>
2021-07-14 00:14:22 +0300

    Update task.js
    Home Work 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,27 +1,36 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age
+  this.name = name;
+  this.gender = gender;
+  this.age = age
 }
 
 const student1 = new Student("Алексей", "мужской", "24");
-const student2= new Student("Алёна", "женский", "23");
+const student2 = new Student("Алёна", "женский", "23");
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 
 Student.prototype.addMark = function (mark) {
-  
-  if(this.marks === undefined){ 
-      this.marks = [];
+  if (this.marks === undefined) {
+    this.marks = [];
     this.marks.push(mark)
   } else {
     this.marks.push(mark)
   }
 }
-// ваш код для остальных методов
-student1.setSubject("Algeb")
-student1.addMark(5)
-student1.addMark(4)
-console.log(student1)
+
+Student.prototype.addMarks = function (...mark) {
+  this.marks = mark;
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce(function (sum, current) {
+    return sum + current;
+  }, 0) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this['marks', 'subject'];
+  this.excluded = reason;
+}
